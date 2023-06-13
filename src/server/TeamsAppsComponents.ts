6773ca068c134cfd38d3bd9443b7985373400ec7
Jun Han <formulahendry@gmail.com>
2023-06-13 17:06:07 +0800

    0

---
+++ src/server/TeamsAppsComponents.ts
@@ -0,0 +1,4 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the tab0613Tab tab
+export * from "./tab0613Tab/Tab0613Tab";
---
+++ src/server/tab0613Tab/Tab0613Tab.ts
@@ -0,0 +1,8 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/tab0613Tab/index.html")
+export class Tab0613Tab {
+}
