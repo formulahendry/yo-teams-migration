b6292f7a6cb332d9f170cae7e97760c5d4c82021
Jun Han <formulahendry@gmail.com>
2023-06-28 15:17:25 +0800

    init bot

---
+++ bot/src/server/botBot/BotBot.ts
@@ -0,0 +1,44 @@
+import { BotDeclaration } from "express-msteams-host";
+import * as debug from "debug";
+import { CardFactory, ConversationState, MemoryStorage, UserState, TurnContext } from "botbuilder";
+import { DialogBot } from "./dialogBot";
+import { MainDialog } from "./dialogs/mainDialog";
+import WelcomeCard from "./cards/welcomeCard";
+
+// Initialize debug logging module
+const log = debug("msteams");
+
+/**
+ * Implementation for bot Bot
+ */
+  @BotDeclaration(
+      "/api/messages",
+      new MemoryStorage(),
+      // eslint-disable-next-line no-undef
+      process.env.MICROSOFT_APP_ID,
+      // eslint-disable-next-line no-undef
+      process.env.MICROSOFT_APP_PASSWORD)
+
+export class BotBot extends DialogBot {
+    constructor(conversationState: ConversationState, userState: UserState) {
+        super(conversationState, userState, new MainDialog());
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            if (membersAdded && membersAdded.length > 0) {
+                for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                    if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                        await this.sendWelcomeCard( context );
+                    }
+                }
+            }
+            await next();
+        });
+    }
+
+    public async sendWelcomeCard( context: TurnContext ): Promise<void> {
+        const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
+        await context.sendActivity({ attachments: [welcomeCard] });
+    }
+
+}
